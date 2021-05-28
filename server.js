07f0aed9d80c151411e398bf8f54c932577ecc1a
DjairCosta <costadjaircloud@gmail.com>
2021-05-28 19:17:01 -0300

    edit sever

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
-console.log("Name for database MongoDB", process.env.MONGO_DB);
-console.log("Name for user MongoDB", process.env.MONGO_USER);
+//console.log("Name for database MongoDB", process.env.MONGO_DB);
+//console.log("Name for user MongoDB", process.env.MONGO_USER);
 
 
-app.get('/', (req, res) => {
-  res.json({
-    'success': true
+//app.get('/', (req, res) => {
+//res.json({
+//'success': true
 
-  });
+//});
 
-});
+//});
 
 
 app.use('/api', api);
